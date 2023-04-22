d3010e2684cdb2143c063efd5d1a89c6947f1e41
Vlad <vldqq@outlook.com>
2023-04-22 17:44:18 +0300

    implement authentication usecase

---
+++ task-auth/tasker/usecase/usecase.go
@@ -0,0 +1,79 @@
+package usecase
+
+import (
+	"errors"
+	"time"
+
+	"github.com/vladqstrn/tasker-auth/task-auth/models"
+	"github.com/vladqstrn/tasker-auth/task-auth/tasker/repo"
+	"github.com/vladqstrn/tasker-auth/task-auth/utils"
+)
+
+var (
+	ErrUserNotFound = errors.New("user not found")
+)
+
+type Auth interface {
+	Register(user *models.User) error
+	Login(user *models.User) error
+	GetUser(user string) (*models.User, error)
+}
+
+type AuthUsecase struct {
+	auth Auth
+	repo *repo.UserRepository
+}
+
+func NewAuthUsecase(repo *repo.UserRepository) *AuthUsecase {
+	return &AuthUsecase{repo: repo}
+}
+
+func (a *AuthUsecase) Register(user *models.User) error {
+	userExists, err := a.repo.GetUserByUsername(user.Username)
+	if err == ErrUserNotFound {
+
+		h, err := utils.HashPassword(user.Password)
+		if err != nil {
+			return err
+		}
+		user.Password = h
+
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		err = a.repo.CreateUser(user)
+		if err != nil {
+			return err
+		}
+	} else {
+		if err != nil {
+			return err
+		}
+	}
+	if userExists != nil {
+		return errors.New("username already taken")
+	}
+
+	return nil
+}
+
+func (a *AuthUsecase) Login(user *models.User) error {
+	usr, err := a.repo.GetUserByUsername(user.Username)
+	if err != nil {
+		return err
+	}
+
+	err = utils.CheckPassHash(user.Password, usr.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *AuthUsecase) GetUser(user string) (*models.User, error) {
+	usr, err := a.repo.GetUserByUsername(user)
+	if err != nil {
+		return nil, err
+	}
+	return usr, nil
+}
